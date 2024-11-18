60b75b8aae9182b04bc2433d0366a6923dab098d
JohnUllaguari <johjaull@espol.edu.ec>
2024-11-18 00:01:53 -0500

    Rediseñado de ventanas generar y añadir

--- ProyectoEstructuras/src/main/java/com/mycompany/proyectoestructuras/controller/AñadirVentanaController.java
+++ ProyectoEstructuras/src/main/java/com/mycompany/proyectoestructuras/controller/AñadirVentanaController.java
@@ -6,7 +6,16 @@ package com.mycompany.proyectoestructuras.controller;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.ImagePattern;
+import javafx.scene.shape.Circle;
 
 /**
  * FXML Controller class
@@ -14,13 +23,67 @@ import javafx.fxml.Initializable;
  * @author flsan
  */
 public class AñadirVentanaController implements Initializable {
-
+    
+    
+    
+    @FXML
+    Circle añadirTelefono;
+    @FXML
+    Circle foto;
+    @FXML
+    Circle cancelar;
+    @FXML
+    Button guardar;
+    @FXML
+    TextField TfNombre;
+    @FXML
+    TextField TfApellido;
+    @FXML
+    TextField TfEmpresa;
+    @FXML
+    TextField TfTelefono;
+    @FXML
+    VBox contTelefonosExtra;
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
+        Image img1 = new Image(getClass().getResource("/com/mycompany/proyectoestructuras/images/cancelar.png").toExternalForm());
+        Image img2 = new Image(getClass().getResource("/com/mycompany/proyectoestructuras/images/añadir.png").toExternalForm());
+        Image img3 = new Image(getClass().getResource("/com/mycompany/proyectoestructuras/images/añadirFoto.png").toExternalForm());
+        cancelar.setFill(new ImagePattern(img1));
+        añadirTelefono.setFill(new ImagePattern(img2));
+        foto.setFill(new ImagePattern(img3));
+        
+        añadirTelefono.setOnMouseClicked(event -> {
+                añadirTelefonos();
+            });
+    }
+    
+    @FXML
+    private void añadirTelefonos() {
+        // Crear un nuevo HBox para el nuevo número de teléfono
+        HBox nuevoTelefono = new HBox();
+        nuevoTelefono.setAlignment(Pos.CENTER);
+        
+        
+        // Crear el texto y el campo de texto
+        TextField textFieldTelefono = new TextField();
+        textFieldTelefono.setPromptText("Teleono");
+        textFieldTelefono.setStyle(
+            "-fx-background-radius: 15; " +
+            "-fx-border-radius: 15; " +
+            "-fx-border-color: #1e90ff;"
+        );
+        
+        // Agregar los elementos al HBox
+        nuevoTelefono.getChildren().addAll(textFieldTelefono);
+        
+        // Añadir el HBox al VBox contenedor
+        contTelefonosExtra.setSpacing(10);
+        contTelefonosExtra.getChildren().add(nuevoTelefono);
+    }
+    
     
 }
--- ProyectoEstructuras/src/main/java/com/mycompany/proyectoestructuras/structures/ListaContactos.java
+++ ProyectoEstructuras/src/main/java/com/mycompany/proyectoestructuras/structures/ListaContactos.java
@@ -14,62 +14,62 @@ import java.util.function.Predicate;
  *
  * @author maza-
  */
-public class ListaContactos {
-    private MyCircleDoubleLinkedList<Contact> contactos;
-    
-    public ListaContactos(){
-        this.contactos = new MyCircleDoubleLinkedList<>();
-    }
-    
-    public void agregarContacto(Contact contacto){
-        contactos.add(contacto);
-    }
-    
-    public boolean eliminarContacto(Contact contacto){
-        if(contactos.isEmpty()){
-            return false;
-        }
-        if(contactos.contains(contacto)){
-            eliminarNodo(contacto);
-            return true;
-        }
-        return false;
-    }
-    
-    private void eliminarNodo(Contact contacto){
-        Contact primero = contactos.getFirst();
-        CircularDoubleNode<Contact> actual = null;
-        actual.setData(primero);
-        do{
-            if(actual.getData().equals(contacto)){
-                CircularDoubleNode<Contact> previo = actual.getPrevious();
-                CircularDoubleNode<Contact> siguiente = actual.getNext();
-                
-                previo.setNext(siguiente);
-                siguiente.setPrevious(previo);
-                
-                if(actual)
-            }
-        }
-    }
-    
-    
-    
-    public boolean contieneContacto(Contact contacto){
-        return contactos.contains(contacto);
-    }
-    
-    public Iterator<Contact> getIterator() {
-        return new IteratorCircular<>(contactos.getFirstNode());      
-    }
-    
-    public List<Contact> filtrarContactos(Predicate<Contact> criterio) {
-        // Método para filtrar contactos basado en un criterio específico
-        return contactos.stream().filter(criterio).toList();
-    }
-    
-    public void ordenarContactos(Comparator<Contact> comparador) {
-        // Puedes implementar el ordenamiento basado en el criterio
-        // Requiere convertir la lista en un formato que soporte ordenar
-    }
-}
+//public class ListaContactos {
+//    private MyCircleDoubleLinkedList<Contact> contactos;
+//    
+//    public ListaContactos(){
+//        this.contactos = new MyCircleDoubleLinkedList<>();
+//    }
+//    
+//    public void agregarContacto(Contact contacto){
+//        contactos.add(contacto);
+//    }
+//    
+//    public boolean eliminarContacto(Contact contacto){
+//        if(contactos.isEmpty()){
+//            return false;
+//        }
+//        if(contactos.contains(contacto)){
+//            eliminarNodo(contacto);
+//            return true;
+//        }
+//        return false;
+//    }
+//    
+//    private void eliminarNodo(Contact contacto){
+//        Contact primero = contactos.getFirst();
+//        CircularDoubleNode<Contact> actual = null;
+//        actual.setData(primero);
+//        do{
+//            if(actual.getData().equals(contacto)){
+//                CircularDoubleNode<Contact> previo = actual.getPrevious();
+//                CircularDoubleNode<Contact> siguiente = actual.getNext();
+//                
+//                previo.setNext(siguiente);
+//                siguiente.setPrevious(previo);
+//                
+//                if(actual)
+//            }
+//        }
+//    }
+//    
+//    
+//    
+//    public boolean contieneContacto(Contact contacto){
+//        return contactos.contains(contacto);
+//    }
+//    
+//    public Iterator<Contact> getIterator() {
+//        return new IteratorCircular<>(contactos.getFirstNode());      
+//    }
+//    
+//    public List<Contact> filtrarContactos(Predicate<Contact> criterio) {
+//        // Método para filtrar contactos basado en un criterio específico
+//        return contactos.stream().filter(criterio).toList();
+//    }
+//    
+//    public void ordenarContactos(Comparator<Contact> comparador) {
+//        // Puedes implementar el ordenamiento basado en el criterio
+//        // Requiere convertir la lista en un formato que soporte ordenar
+//    }
+//}
